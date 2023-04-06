3486d339938126633afed9f1a1e0d72f7f1fd828
Daaanxx <dannxko@gmail.com>
2023-04-06 18:25:36 +0100

    Adding empty files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add your own API key between the ""
+var APIKey = "6b993bd38295772f03036f665669b708";
+
+// Here we are building the URL we need to query the database
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
+  APIKey;
